9048fe70027b4b0102a762fa863edf5378fea27e
vishalprasanna11 <prasanna.vi@northeastern.edu>
2024-09-26 13:04:31 -0400

    Update the Router Response

--- src/controllers/health.Controller.ts
+++ src/controllers/health.Controller.ts
@@ -4,22 +4,22 @@ import sequelize  from '../config/database' ;
 const healthCheck = async (req: Request, res: Response) => {
 
   if (req.body && JSON.stringify(req.body) != '{}') {
-    return res.status(400).json({message:'Bad Request: No payload allowed'});
+    return res.status(400).send();
   }
 
   try {
     if (!sequelize) {
       res.setHeader('Cache-Control', 'no-cache');
-      return res.status(503).json({message:'Service Unavailable'}); 
+      return res.status(503).send(); 
     }
 
     await sequelize.sync();  
     res.setHeader('Cache-Control', 'no-cache');
-    return res.status(200).json({message:'OK'}); 
+    return res.status(200).send(); 
 
   } catch (error) {
     res.setHeader('Cache-Control', 'no-cache');
-    return res.status(503).json({message:'Service Unavailable'});  
+    return res.status(503).send();  
   }
 };
 
